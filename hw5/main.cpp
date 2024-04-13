766d4ae14ea862c17638366af190081b68279159
juno5626 <junono1@naver.com>
2024-04-13 23:19:36 +0900

    .

---
+++ hw5/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#define SIZE 20 // Maximum size of the stack
+using namespace std;
+
+class Stack {
+private:
+    int top;
+    char sta[SIZE]; 
+
+public:
+    Stack() : top(0) {}
+    void push(char a) {
+        sta[top++] = a;
+    }
+
+    char pop() {
+        return sta[--top];
+    }
+
+    bool isEmpty() const {
+        return top == 0;
+    }
+
+    void Print() const {
+        if (isEmpty()) {
+            cout << "Stack is empty" << std::endl;
+            return;
+        }
+
+        cout << "Stack:";
+        for (int i = 0; i < top; ++i) {
+            cout << " " << sta[i];
+        }
+        cout << std::endl;
+    }
+};
+
+int main() {
+    Stack stack1;
+    char temp;
+
+    while(1){
+        cout<<"Enter the character you want to push into the stack.(0:stop)"<<endl;
+        cin>>temp;
+        if(temp == '0'){
+            break;
+        }
+        stack1.push(temp);
+    }
+    stack1.Print(); 
+
+    return 0;
+}
